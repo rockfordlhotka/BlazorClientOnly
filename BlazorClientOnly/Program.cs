89517280ce58d8336de811cefeae60f223f37904
Rockford Lhotka <rocky@lhotka.net>
2020-04-24 10:32:30 -0500

    Update for Blazor 3.2.0-preview5.20216.8

--- BlazorClientOnly/Program.cs
+++ BlazorClientOnly/Program.cs
@@ -1,22 +1,21 @@
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
 
 namespace BlazorClientOnly
 {
-    public class Program
+  public class Program
+  {
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+      var builder = WebAssemblyHostBuilder.CreateDefault(args);
+      builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddBaseAddressHttpClient();
+      builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            await builder.Build().RunAsync();
-        }
+      await builder.Build().RunAsync();
     }
+  }
 }
